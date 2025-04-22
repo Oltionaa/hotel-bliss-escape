c947532215989f2f1812b54aaf7b2f33832cb64a
vesadakaj <vesadakaj345@gmail.com>
2025-04-22 15:22:46 +0200

    test

---
+++ frontend/src/RoomController.js
@@ -0,0 +1,6 @@
+<div className="container bg-light py-4 mt-n5 position-relative" style={{ marginTop: '-50px' }}>
+        <div className="row g-2">
+          <div className="col-md">
+            <label className="form-label">Check in</label>
+            <input type="date" className="form-control" />
+          </div>
